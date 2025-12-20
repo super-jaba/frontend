be9dbdcc66d0b889dfc8ea3c6c12130c382d3d7d
Pasha Kononov <kononoffp@gmail.com>
2025-12-20 15:42:47 -0500

    Go to document from reference

--- src/4_entities/References/ui/ReferenceCard.tsx
+++ src/4_entities/References/ui/ReferenceCard.tsx
@@ -1,5 +1,9 @@
 import type { Reference } from '../model/types'
-import { Card, Typography } from 'antd'
+import { Card, Typography, Dropdown } from 'antd'
+import type { MenuProps } from 'antd'
+import { MoreOutlined, FileTextOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import { RoutePath } from '@/5_shared/config/routerConfig'
 import cls from './ReferenceCard.module.css'
 
 const { Text } = Typography
@@ -11,8 +15,42 @@ interface Props {
 export const ReferenceCard = (props: Props) => {
     const { reference } = props
 
+    const menuItems: MenuProps['items'] = reference.document_id
+        ? [
+              {
+                  key: 'go-to-document',
+                  label: (
+                      <Link
+                          to={RoutePath.document.replace(
+                              ':id',
+                              reference.document_id,
+                          )}
+                      >
+                          Go to document
+                      </Link>
+                  ),
+                  icon: <FileTextOutlined />,
+              },
+          ]
+        : []
+
     return (
         <Card className={cls.referenceCard}>
+            {reference.document_id && (
+                <Dropdown
+                    menu={{ items: menuItems }}
+                    trigger={['click']}
+                    placement="bottomRight"
+                >
+                    <MoreOutlined
+                        className={cls.moreIcon}
+                        onClick={(e) => {
+                            e.preventDefault()
+                            e.stopPropagation()
+                        }}
+                    />
+                </Dropdown>
+            )}
             {reference.reference_type && (
                 <Text type="secondary" className={cls.referenceType}>
                     {reference.reference_type.replace(/_/g, ' ')}
